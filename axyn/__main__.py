451f92325b51eeb8014b311a0659bf13f95e2dcc
Daniel Thwaites <danthwaites30@btinternet.com>
2023-01-23 10:20:15 +0000

    Don't respond with slurs :speech_balloon:

--- axyn/__main__.py
+++ axyn/__main__.py
@@ -23,6 +23,11 @@ def generate_message(generator, input_messages):
                 # Nothing was generated in the current iteration
                 return messages[len(input_messages)]
 
+        for stop_word in {"nigga", "nigger", "nig nog", "fag", "whore", "slut"}:
+            if stop_word in result_text:
+                # Try again for a less offensive result
+                return generate_message(generator, input_messages)
+
         messages = result_text.split("\n")
 
     return messages[len(input_messages)]
